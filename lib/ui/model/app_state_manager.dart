cd2516c757d80d081fe4804c529a42cf73cdc1b6
daniel adame <skaad102@gmail.com>
2022-10-21 09:36:50 -0500

    doc state app

--- lib/ui/model/app_state_manager.dart
+++ lib/ui/model/app_state_manager.dart
@@ -1,48 +1,65 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AppStateManager extends ChangeNotifier {
   bool _initialized = false;
   bool _loggedIn = false;
+  bool _micOn = false;
 
   bool get isInitialized => _initialized;
   bool get isLoggedIn => _loggedIn;
+  bool get isMicOn => _micOn;
 
+  /// It waits for 2 seconds and then sets the _initialized variable to true.
+  /// This is just to simulate a loading screen.
   void initializeApp() {
-    // 7
-    Timer(
-      const Duration(milliseconds: 2000),
-      () {
-        // 8
-        _initialized = true;
-        // 9
-        notifyListeners();
-      },
-    );
+    Timer(const Duration(milliseconds: 2000), () {
+      _initialized = true;
+      notifyListeners();
+    });
   }
 
+  /// It takes two strings, prints them, and then notifies all listeners that the state has changed
+  ///
+  /// Args:
+  ///   phone (String): The phone number of the user
+  ///   pin (String): The user's pin
   void login(String phone, String pin) {
-    // 10
-    // _loggedIn = true;
     print({phone, pin});
-    // 11
     notifyListeners();
   }
 
-  void goTo(String ruta) {
-    print('proxima pagina');
-    // 12
-    notifyListeners();
-  }
-
+  /// The function sets the _loggedIn variable to false, sets the _initialized variable to false, sets the
+  /// _micOn variable to false, calls the initializeApp() function, and then calls the notifyListeners()
+  /// function
   void logout() {
-    // 12
     _loggedIn = false;
     _initialized = false;
-    // 13
+    _micOn = false;
     initializeApp();
-    // 14
+    notifyListeners();
+  }
+
+  /// If the user has granted the microphone permission, toggle the microphone on/off. If the user has
+  /// denied the microphone permission, open the app settings
+  void toggleMic() async {
+    final status = await Permission.microphone.request();
+    switch (status) {
+      case PermissionStatus.granted:
+        toggleMicOnOff();
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.permanentlyDenied:
+      case PermissionStatus.restricted:
+      case PermissionStatus.limited:
+        openAppSettings();
+    }
+  }
+
+  void toggleMicOnOff() {
+    _micOn = !_micOn;
     notifyListeners();
   }
 }
