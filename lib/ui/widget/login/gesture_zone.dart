486df7ae8f3be5b449b8ba20ebfddec4f504c971
daniel adame <skaad102@gmail.com>
2022-10-31 06:47:56 -0500

    refact gesture zone

--- lib/ui/widget/login/gesture_zone.dart
+++ lib/ui/widget/login/gesture_zone.dart
@@ -1,42 +1,104 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:prueba_a_kasa/ui/theme/theme.dart';
 
+import '../../model/app_state_manager.dart';
+
 class ZonaGestos extends StatelessWidget {
-  bool activo;
-  ZonaGestos({this.activo = true, super.key});
+  @override
+  Widget build(BuildContext context) {
+    final zonaManager = Provider.of<ZonaGestosManager>(context);
+    final zonaOnOff = zonaManager.zoneGesture;
+    return GestureDetector(
+      // /* activar o desactivar zona de gestos */
+      // onPanStart: (_) => zonaManager.timerCancel(),
+      // onPanUpdate: (details) => {
+      //   if (details.delta.direction == 0 && zonaOnOff)
+      //     {
+      //       print("Derecha"),
+      //     }
+      //   else if (details.delta.direction > 1 && zonaOnOff)
+      //     {
+      //       print("Izquierda"),
+      //     }
+      // },
+      onPanEnd: (_) => zonaManager.timerCancel(),
+      onPanDown: (_) {
+        zonaOnOff
+            ? zonaManager.changeZoneGesture()
+            : zonaManager.changeZoneGestureOff();
+      },
+      child: ContentGestureZone(zonaOnOff: zonaOnOff),
+    );
+  }
+}
+
+class ContentGestureZone extends StatelessWidget {
+  ContentGestureZone({
+    Key? key,
+    required this.zonaOnOff,
+  }) : super(key: key);
+
+  final bool zonaOnOff;
+  late Color colorBg;
+  late Color colorBd;
+  @override
+  Widget build(BuildContext context) {
+    /* Colores */
+    zonaOnOff ? colorBg = ThemeAKasa.btnZone : colorBg = ThemeAKasa.btnZoneOff;
+
+    zonaOnOff
+        ? colorBd = ThemeAKasa.btnBorderZone
+        : colorBd = ThemeAKasa.btnBorderZoneOff;
+    ///////////////////////////////S
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 19),
+      width: 328,
+      height: 190,
+      decoration: BoxDecoration(
+        color: colorBg,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: colorBd,
+          width: 2,
+        ),
+      ),
+      child: zonaOnOff
+          ? GestureZoneText(
+              subText: "Presiona 5 Segundos para desactivar",
+              colorBd: colorBd,
+            )
+          : GestureZoneText(
+              subText: "Presiona 2 Segundos para activar",
+              colorBd: colorBd,
+            ),
+    );
+  }
+}
+
+class GestureZoneText extends StatelessWidget {
+  String subText;
+  Color colorBd;
+  GestureZoneText({
+    required this.subText,
+    required this.colorBd,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.symmetric(vertical: 19),
-        width: 328,
-        height: 190,
-        decoration: BoxDecoration(
-          color: ThemeAKasa.btnZone,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: ThemeAKasa.btnBorderZone,
-            width: 2,
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          "Zona de gestos",
+          style: TextStyle(
+            color: colorBd,
           ),
         ),
-        child: activo
-            ? Center(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  Text(
-                    "Zona de gestos",
-                    style: TextStyle(
-                      color: ThemeAKasa.btnBorderZone,
-                    ),
-                  ),
-                  Text("Aqui se mostraran los gestos"),
-                ],
-              ))
-            : const Center(
-                child: Text("No activo"),
-              ));
+        Text(subText),
+      ],
+    ));
   }
 }
