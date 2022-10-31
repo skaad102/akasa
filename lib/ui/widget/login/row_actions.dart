c382e0e983ab958403d7e987f500a00accf8b2b1
daniel adame <skaad102@gmail.com>
2022-10-31 14:58:17 -0500

    refact method micro

--- lib/ui/widget/login/row_actions.dart
+++ lib/ui/widget/login/row_actions.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:prueba_a_kasa/services/SpeechApi/speech_api.dart';
 
-import '../../model/app_state_manager.dart';
+import '../../../services/provider/app_state_manager.dart';
 import '../../theme/theme.dart';
 import '../btn_actions.dart';
-
-import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 /* Botones para las diferentes acciones en la aplicacion en la vista login */
 class RowBtnActions extends StatefulWidget {
@@ -17,18 +16,8 @@ class RowBtnActions extends StatefulWidget {
 }
 
 class _RowBtnActionsState extends State<RowBtnActions> {
-  var _spechText = stt.SpeechToText();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _spechText = stt.SpeechToText();
-  }
-
   @override
   Widget build(BuildContext context) {
-    final appState = Provider.of<AppStateManager>(context, listen: false);
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
@@ -53,7 +42,8 @@ class _RowBtnActionsState extends State<RowBtnActions> {
           textColor: ThemeAKasa.btnBorderHear,
           textAction: getIconTxt(context),
           icon: getIconMic(context),
-          onAction: () => listenToMe(appState.isMicOn, context),
+          // onAction: () => listenToMe(appState.isMicOn, context),
+          onAction: () => SpeechApi.listenToMe(context),
           msSemantic:
               "Dí telefono seguido de tu número, posteriormente dí contraseña seguido de tu pin",
         ),
@@ -69,50 +59,14 @@ class _RowBtnActionsState extends State<RowBtnActions> {
     );
   }
 
-  void listenToMe(bool micOn, BuildContext context) async {
-    final appState = Provider.of<AppStateManager>(context, listen: false);
-    appState.toggleMic();
-    final micOn = appState.isMicOn;
-    String text = appState.vozToText;
-    if (!micOn) {
-      bool available = await _spechText.initialize(
-        // onStatus: (val) => print('onStatus: $val'),
-        onStatus: (val) {
-          if (val == 'notListening') {
-            appState.toggleMicOnOff();
-          }
-        },
-        onError: (val) => print('onError: $val'),
-      );
-      if (available) {
-        _spechText.listen(
-          cancelOnError: true,
-          onResult: (val) => setState(() {
-            text = val.recognizedWords;
-            appState.setVozToText(text);
-          }),
-        );
-      }
-    } else {
-      _spechText.stop();
-      // text = "";
-      appState.splitText();
-    }
-  }
-
   IconData getIconMic(BuildContext context) {
     final microOn = Provider.of<AppStateManager>(context).isMicOn;
-    if (microOn) return Icons.mic;
-    return Icons.mic_off;
+    return microOn ? Icons.mic : Icons.mic_off;
   }
 
   String getIconTxt(BuildContext context) {
     final microOn = Provider.of<AppStateManager>(context).isMicOn;
     if (!microOn) return "Te Escucho";
     return "No Escuchar";
-  }
-
-  void permiso() {
-    print("Permiso");
   }
 }
